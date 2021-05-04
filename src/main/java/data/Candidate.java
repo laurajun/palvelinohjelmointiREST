f01dd019a8260f1b866cc6667a7ef318448da7e7
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-05-04 21:21:06 +0300

    Add new getters and setters

--- src/main/java/data/Candidate.java
+++ src/main/java/data/Candidate.java
@@ -24,24 +24,24 @@ public class Candidate {
 	}
 	public Candidate(int candidateNr, String party, String firstname, String lastname, String streetAddr, String zipCode, String city, String notes) {
 		this.candidateNr=candidateNr;
-		this.party=party;
-		this.firstname=firstname;
-		this.lastname=lastname;
-		this.streetAddr=streetAddr;
-		this.zipCode=zipCode;
-		this.city=city;
-		this.notes=notes;
+		this.setParty(party);
+		this.setFirstname(firstname);
+		this.setLastname(lastname);
+		this.setStreetAddr(streetAddr);
+		this.setZipCode(zipCode);
+		this.setCity(city);
+		this.setNotes(notes);
 	}
 	
 	public Candidate(String candidateNr, String party, String firstname, String lastname, String streetAddr, String zipCode, String city, String notes) {
 		this.setCandidateNr(candidateNr);
-		this.party=party;
-		this.firstname=firstname;
-		this.lastname=lastname;
-		this.streetAddr=streetAddr;
-		this.zipCode=zipCode;
-		this.city=city;
-		this.notes=notes;
+		this.setParty(party);
+		this.setFirstname(firstname);
+		this.setLastname(lastname);
+		this.setStreetAddr(streetAddr);
+		this.setZipCode(zipCode);
+		this.setCity(city);
+		this.setNotes(notes);
 	}
 	public int getId() {
 		return id;
@@ -70,5 +70,47 @@ public class Candidate {
 		catch(NumberFormatException | NullPointerException e) {
 			//Do nothing - the value is not changed
 		}
+	}
+	public String getParty() {
+		return party;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getStreetAddr() {
+		return streetAddr;
+	}
+	public void setStreetAddr(String streetAddr) {
+		this.streetAddr = streetAddr;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
 	}
 }
