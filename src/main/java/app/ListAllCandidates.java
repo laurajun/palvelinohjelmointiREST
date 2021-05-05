66895fcc9b275b8789b4eaa1be9392fcc0b94c81
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-05-05 19:15:31 +0300

    Create ListAllCandidates servlet and JSP

---
+++ src/main/java/app/ListAllCandidates.java
@@ -0,0 +1,67 @@
+package app;
+
+import java.io.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+
+import java.util.ArrayList;
+import java.util.List;
+import data.*;
+
+/**
+ * Servlet implementation class ListAllCandidates
+ */
+@WebServlet("/ListAllCandidates")
+public class ListAllCandidates extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListAllCandidates() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = "http://127.0.0.1:8080/rest/vaalikoneservice/listAllCandidatesService";
+
+		Client asiakas=ClientBuilder.newClient();
+		WebTarget wt=asiakas.target(uri);
+		Builder b=wt.request();
+		
+		//Create a GenericType to be able to get List of objects
+		//This will be the second parameter of post method
+		GenericType<List<Candidate>> genericList = new GenericType<List<Candidate>>() {};
+		
+		//Posting data (Entity<ArrayList<DogBreed>> e) to the given address
+		List<Candidate> returnedList=b.get(genericList);
+	    request.setAttribute("candidatelist", returnedList);
+	    RequestDispatcher rd=request.getRequestDispatcher("./jsp/listallcandidates.jsp");
+		rd.forward(request, response);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/jsp/listallcandidates.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>     
+<%@include  file="../htmlstart.html" %>
+<table border="0" class="tabledb">
+	<tr>
+		<td><b>ID</b></td><td><b>Etunimi</b></td><td><b>Sukunimi</b></td><td><b>Puolue</b></td><td><b>Toiminnot</b></td>
+	</tr>
+<c:forEach var="candidate" items="${requestScope.candidatelist }">
+<tr>
+	<td>${candidate.id}</td>
+	<td>${candidate.firstname}</td>
+	<td>${candidate.lastname}</td>
+	<td>${candidate.party}</td>
+	<td><a href="/ShowSingleCandidate?id=${candidate.id}">Näytä</a></td>
+</tr>
+</c:forEach>
+</table>
+<%@include  file="../htmlend.html" %>
