26e9f07fc72787828b5735af230497b4d15a1617
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-05-10 20:56:55 +0300

    Create fileupload method

--- src/main/java/services/Vaalikone.java
+++ src/main/java/services/Vaalikone.java
@@ -1,11 +1,17 @@
 package services;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -14,7 +20,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import data.Candidate;
 
@@ -118,10 +130,32 @@ public class Vaalikone {
 			em.getTransaction().commit();
 			return c;
 		}
-			
-//	private ArrayList<Candidate> getCandidates() {
-//		// TODO Auto-generated method stub
-//		ArrayList<Candidate> list=new ArrayList<>();
-//		return list;
-//	}
+		
+		//This method does the file upload
+		@POST
+		@Path("/fileupload")
+		@Consumes({MediaType.MULTIPART_FORM_DATA})
+		public Response uploadFile( @FormDataParam("file") InputStream fileInputStream,
+	            @FormDataParam("file") FormDataContentDisposition fileMetaData, @Context ServletContext sc) 
+	            		throws Exception
+		{
+			String UPLOAD_PATH=sc.getRealPath("/");
+		    try{
+		        int read = 0;
+		        byte[] bytes = new byte[1024];
+		 
+		        OutputStream out = new FileOutputStream(new File(UPLOAD_PATH + "/"+fileMetaData.getFileName()));
+		        while ((read = fileInputStream.read(bytes)) != -1) 
+		        {
+		            out.write(bytes, 0, read);
+		        }
+		        out.flush();
+		        out.close();
+		        
+		    } 
+		    catch (IOException e){
+		        throw new WebApplicationException("Error while uploading file. Please try again !!");
+		    }
+		    return Response.ok("Data uploaded successfully !!").build();
+		}
 }
